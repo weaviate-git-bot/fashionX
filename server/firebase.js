e974a1e3bb472e8737db68c6ccb4c1164d8834bc
saswatsam786 <saswat.sam786@gmail.com>
2023-08-12 10:57:36 +0530

    added firebase to server

---
+++ server/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD2uaLHyNB4F5VpUTfLyFFQobDrySAZtsw",
+    authDomain: "fashion-x-c72e0.firebaseapp.com",
+    projectId: "fashion-x-c72e0",
+    storageBucket: "fashion-x-c72e0.appspot.com",
+    messagingSenderId: "921742120513",
+    appId: "1:921742120513:web:64246da69de0e5380798c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
