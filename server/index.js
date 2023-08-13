363c12eafec8b7531fe3014d84a6b5b3d5bad492
Avash027 <avashmitra2706@gmail.com>
2023-08-13 10:22:07 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import weaviate from 'weaviate-client';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import cors from "cors"
+import bodyParser from "body-parser"
 import { db } from "./firebase.js"
 
 dotenv.config();
@@ -15,7 +17,7 @@ const client = weaviate.client({
 })
 
 const schemaConfig = {
-    'class': 'Fashion',
+    'class': 'FashionY',
     'vectorizer': 'img2vec-neural',
     'vectorIndexType': 'hnsw',
     'moduleConfig': {
@@ -30,7 +32,7 @@ const schemaConfig = {
             'description': 'The image of the fashion item',
         },
         {
-            'name': 'productID',
+            'name': 'product_id',
             'dataType': ['string'],
             'description': 'The product ID of the fashion item',
         }
@@ -54,15 +56,22 @@ async function createSchema() {
 
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json({ limit: "50mb" }))
 
 // @TODO Change it after the frontend is ready
 
 app.get('/api', async (req, res) => {
     const test = Buffer.from(fs.readFileSync('./test/test.jpg')).toString('base64');
+
+    fs.writeFileSync('./test/test.txt', test);
+
+
+
     const resImage = await client
         .graphql
         .get()
-        .withClassName("Fashion")
+        .withClassName("FashionY")
         .withFields(['image'])
         .withNearImage({ image: test })
         .withLimit(2)
@@ -83,6 +92,49 @@ app.get('/api', async (req, res) => {
 
 });
 
+app.get("/frontpage/:count", async (req, res) => {
+    //get random images from the database
+
+    const count = req.params.count
+    const data = await db.collection("metadata").get()
+    const images = data.docs.map(doc => doc.data())
+    const randomImages = []
+    for (let i = 0; i < count; i++) {
+        const randomIndex = Math.floor(Math.random() * images.length)
+        randomImages.push(images[randomIndex])
+    }
+    res.send(randomImages)
+})
+
+app.post("/related-products/:cnt", async (req, res) => {
+    const { cnt } = req.params
+    const { base64Image } = req.body
+
+
+    const resImage = await client
+        .graphql
+        .get()
+        .withClassName("FashionY")
+        .withFields(['image', 'product_id'])
+        .withNearImage({ image: String(base64Image) })
+        .withLimit(cnt)
+        .do()
+
+    const products = []
+
+    for (let i = 0; i < resImage.data.Get.FashionY.length; i++) {
+        const productID = resImage.data.Get.FashionY[i].product_id
+        console.log(productID)
+        const productData = await db.collection("metadata").where("productID", "==", productID).get()
+        const product = productData.docs[0].data()
+        products.push(product)
+
+    }
+
+    res.send(products)
+})
+
+
 const PORT = process.env.PORT || 5000;
 
 async function loadImages() {
@@ -99,7 +151,7 @@ async function loadImages() {
         const fileData = fs.readFileSync(`${dir}/${file}`);
         const bg4 = Buffer.from(fileData).toString('base64');
         const res = await client.data.creator().withClassName("Fashion")
-            .withProperties({ image: bg4, productID: file })
+            .withProperties({ image: bg4, "product_id": file })
             .do()
 
         console.log(res)
@@ -116,33 +168,32 @@ createSchema()
 
 
 const temp = async () => {
+
     const dir = "./images"
-
     const files = fs.readdirSync(dir);
-
     const newfiles = files.filter(file => !file.endsWith(".json")).map(file => file.substring(0, file.lastIndexOf(".")))
+
 
     newfiles.forEach(async (file) => {
 
-        const imageMetaData = fs.readFileSync(`${dir}/${file}.json`);
-        const imageMetaDataJson = JSON.parse(imageMetaData);
-        imageMetaDataJson.productID = file.substring(0, file.lastIndexOf(".json"))
+        // const imageMetaData = fs.readFileSync(`${dir}/${file}.json`);
+        // const imageMetaDataJson = JSON.parse(imageMetaData);
+        // imageMetaDataJson.productID = file
 
-        db.collection("metadata").add(imageMetaDataJson)
-
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        // db.collection("metadata").add(imageMetaDataJson)
+        console.log(file)
 
         const imageData = fs.readFileSync(`${dir}/${file}.jpg`);
         const bg4 = Buffer.from(imageData).toString('base64');
-        const res = await client.data.creator().withClassName("Fashion")
-            .withProperties({ image: bg4, productID: file.substring(0, file.lastIndexOf(".json")) })
+        const res = await client.data.creator().withClassName("FashionY")
+            .withProperties({ image: bg4, "product_id": file })
             .do()
 
         console.log(res)
     })
 }
 
-loadImages()
+// loadImages()
 // temp()
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
